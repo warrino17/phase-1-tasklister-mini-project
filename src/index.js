c79c0815bd606e25ca4a5807fda5f0328362b19e
Warren Zhang <warrenzhang17@gmail.com>
2023-03-23 15:49:04 -0400

    pain

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.getElementById("create-task-form");
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+  });
+  
+    const button = document.getElementById('create-button');
+    button.addEventListener('click', function() {
+      const taskInput = document.getElementById('new-task-description');
+      const taskString = taskInput.value;
+      const taskList = document.getElementById('tasks');
+      const newTask = document.createElement('li');
+      newTask.innerText = taskString;
+      taskList.appendChild(newTask);
+      taskInput.value = '';
+
+      const deleteButton = document.createElement('button');
+      deleteButton.innerText = 'Delete';
+      deleteButton.addEventListener('click', function(event) {
+        const taskRemove = event.target.parentElement;
+        taskRemove.remove();
+      });
+      newTask.appendChild(deleteButton);
+  
+      taskList.appendChild(newTask);
+      taskInput.value = '';
+    });
+  });
